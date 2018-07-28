ed49ab9132992747dfe3d5494559a8de1f5ecb16
Emmanuel Sackey <esackey15@alustudent.com>
2018-07-28 15:04:31 +0000

    code cleanup. removed unused code in app.js

--- app0.js
+++ app0.js
@@ -1,5 +1,27 @@
 
 
+/*
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+//request.open('GET', 'http://quotes.rest/qod.json?category=management', true);
+
+request.open('GET', 'http://localhost/extension/test.json', true);
+request.responseType = "json"
+
+let data = this.request;
+request.onload = function () {
+  // Begin accessing JSON data here
+  if (request.status >= 200 && request.status < 400) {
+  //  let quote = data.contents.quotes[quote];
+  let quote = request.response;
+  console.log("logging for XMLHttpRequest");
+  console.log(quote);
+    quotesContainter.innerText = quote;
+  }
+  request.send();
+}*/
+
 function getQuote(){
 //  fetch("http://quotes.rest/qod.json?category=management")
   fetch("http://localhost/extension/test.json", {mode:'no-cors'})
@@ -41,6 +63,16 @@ function getTime(){
 }
 
 function getNews(){
+/*  fetch("localhost/extension/rss.xml")
+  .then(function(response){
+    return response.text();
+  })
+  .then(function(data){
+    var parser = new DOMParser();
+    parser.parseFromString(data, "text/xml");
+    console.log(data);
+  })*/
+
   var request = new XMLHttpRequest();
   request.open('GET', 'http://localhost/extension/rss.xml', true);
   request.onload(function(response){
@@ -52,6 +84,20 @@ function getNews(){
     else{
       console.log("Error getting data");
     }
+/*
+      //fetch("http://localhost/extension/rss.xml")
+      fetch("http://localhost/extension/rss.xml", {mode:'no-cors'})
+      .then(function(response){
+       return response.text();
+       })
+       .then(function(data){
+         //get background url
+         var myresponse = data;
+        // console.log("logging for Fetch");
+         console.log(data);
+        // console.log(data["contents"]["quotes"][0]);
+  })*/
+
 
 });
 }
